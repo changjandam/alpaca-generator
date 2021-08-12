4b3e6438840a8549022103c6c835fb7d09c160d4
changjandam <changjandamtw@gmail.com>
2021-08-12 09:12:16 +0800

    finish html structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import blue50 from './alpaca/backgrounds/blue50.png'
+import './App.css'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header>
+        <h1 id='title'>Alpaca generator</h1>
       </header>
-    </div>
-  );
+      <section>
+        <div id="output">
+          <div id="display-alpaca">
+            <img src={blue50} alt="" />
+          </div>
+          <button id="download" className="white">Download</button>
+        </div>
+        <div id="selectors">
+          <div id="parts">
+            <h2 id="parts-title">ACCESSORIZE THE ALPACA&apos;S</h2>
+            <div id="parts-btn-group">
+              <button className="selector">btn</button>
+            </div>
+          </div>
+          <div id="styles">
+            <h2 id="styles-title">STYLE</h2>
+            <div id="styles-btn-group">
+              <button className="selector">btn</button>
+            </div>
+          </div>
+          <button id="random" className="white">Random</button>
+        </div>
+      </section>
+    </main>
+  )
 }
 
-export default App;
+export default App
